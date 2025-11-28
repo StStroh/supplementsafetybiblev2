7ed952741d97e7f2eb2fb00919a4d611285fa9d0
StStroh <sales@certifiednutralabs.com>
2025-11-28 11:29:28 -0500

    Updated search.ts

--- netlify/functions/search.ts
+++ netlify/functions/search.ts
@@ -9,7 +9,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 export const handler: Handler = async (event) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
   };
 
